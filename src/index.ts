0cd3b3e70b097995d377e2a98a31afd80d39f8f2
hanari-teacher <20c133ch@edu.tech.ac.jp>
2026-08-26 16:26:18 +0900

    テンプレートエンジン設定完了

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,22 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.json());
-
+app.use(express.json()); //JSON形式のデータ をサーバー側で読み取れるようにする
+app.use(express.static("./public")); //静的ファイルの指定
+app.set('views','./views'); //ビューのディレクトリを設定
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express + TypeScript World!');
 });
 
+//ルーティングを追加
+app.get('/test', (req: Request, res: Response) => {
+  const data = {
+    'title' : 'タイトル',
+    'lists': ['リスト1','リスト2','リスト2','リスト2']
+  }
+  res.render('test.ejs',data);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
